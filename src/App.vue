529d851b8687a1ac5c0a31dda88a1f0936eb8038
matveybobrov <matveybobrov01@gmail.com>
2025-04-03 17:25:43 +0300

    Add theme changer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import ThemeChanger from './components/common/ThemeChanger.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="relative">
+    <ThemeChanger class="theme-changer" />
+    <RouterView />
+  </div>
 </template>
 
+<style scoped lang='scss'>
+.theme-changer {
+  top: 20px;
+  right: 20px;
+  position: absolute;
+}
+</style>
+
 <style lang='scss'>
+//
+// Global styles
+//
 button {
   cursor: pointer;
 }
--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import { useShadowColor } from '@/composables/useShadowColor'
+
+const { shadowColor } = useShadowColor()
+</script>
+
 <template>
   <div class="flex w-screen h-screen">
     <div class="h-full" />
-    <div class="flex items-center ml-auto p-20 justify-center h-full shadow-md">
+    <div
+      class="flex items-center ml-auto p-20 justify-center h-full shadow-xl"
+      :class="shadowColor"
+    >
       <RouterView />
     </div>
   </div>
